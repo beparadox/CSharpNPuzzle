1f8def7c0b3f873a392dceb282601ec89eb5d8d2
Bambridge E. Peterson <bambridge.peterson@gmail.com>
2016-09-25 15:56:57 -0500

    created a Frontier.cs class for use in graph search algorithms

---
+++ tests/Frontier_Tests.cs
@@ -0,0 +1,16 @@
+namespace NPuzzleTests
+{
+	using System;
+	using System.Collections.Generic;
+
+	[TestFixture]
+	public class TestFrontier
+	{
+		[Test]
+                public void TestFrontierCreation()
+		{
+
+
+		}
+	}
+}
